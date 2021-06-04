cda43c1649b9d5dbce927a4de5b8b5f84de3fd16
tvrda34 <tvrtko.ivasic3@gmail.com>
2021-06-04 15:55:08 +0200

    Search fragment + Loading

---
+++ app/src/main/java/com/example/pokemonsapp/ui/adapters/LoadStateAdapter.kt
@@ -0,0 +1,58 @@
+package com.example.pokemonsapp.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.view.isVisible
+import androidx.paging.LoadState
+import androidx.paging.LoadStateAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.pokemonsapp.R
+import com.example.pokemonsapp.databinding.LoadStateViewBinding
+
+class LoadStateViewHolder(
+    parent: ViewGroup,
+    retry: () -> Unit
+) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context)
+        .inflate(R.layout.load_state_view, parent, false)
+) {
+    private val binding = LoadStateViewBinding.bind(itemView)
+    private val progressBar: ProgressBar = binding.loadStateProgress
+    private val errorMsg: TextView = binding.loadStateErrorMessage
+    private val retry: Button = binding.loadStateRetry
+        .also {
+            it.setOnClickListener { retry() }
+        }
+
+    fun bind(loadState: LoadState) {
+        if (loadState is LoadState.Error) {
+            errorMsg.text = loadState.error.localizedMessage
+        }
+
+        progressBar.isVisible = loadState is LoadState.Loading
+        retry.isVisible = loadState is LoadState.Error
+        errorMsg.isVisible = loadState is LoadState.Error
+    }
+}
+
+// Adapter that displays a loading spinner when
+// state = LoadState.Loading, and an error message and retry
+// button when state is LoadState.Error.
+class PokemonLoadStateAdapter(
+    private val retry: () -> Unit
+) : LoadStateAdapter<LoadStateViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        loadState: LoadState
+    ) = LoadStateViewHolder(parent, retry)
+
+    override fun onBindViewHolder(
+        holder: LoadStateViewHolder,
+        loadState: LoadState
+    ) = holder.bind(loadState)
+}
+
--- app/src/main/java/com/example/pokemonsapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/example/pokemonsapp/ui/search/SearchFragment.kt
@@ -4,17 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadState
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.pokemonsapp.databinding.FragmentSearchBinding
+import com.example.pokemonsapp.ui.adapters.PagedPokemonsAdapter
+import com.example.pokemonsapp.ui.adapters.PokemonLoadStateAdapter
+import com.example.pokemonsapp.viewmodel.SharedViewModel
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 class SearchFragment : Fragment() {
 
     private var _binding: FragmentSearchBinding? = null
+    private val viewModel by activityViewModels<SharedViewModel>()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
+    @ExperimentalPagingApi
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,12 +37,67 @@ class SearchFragment : Fragment() {
 
         _binding = FragmentSearchBinding.inflate(inflater, container, false)
 
+        val pagingAdapter = PagedPokemonsAdapter(onFavClick = {
+            viewModel.onStarClikc(requireContext(), it)
+        })
+
+        val recyclerView = binding.recyclerView
+        recyclerView.setHasFixedSize(false)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = pagingAdapter.withLoadStateFooter(
+            footer = PokemonLoadStateAdapter { pagingAdapter.retry() }
+        )
+
+        /* viewLifecycleOwner.lifecycleScope.launch {
+             viewModel.flow.collectLatest { pagingData ->
+                 pagingAdapter.submitData(viewLifecycleOwner.lifecycle, pagingData)
+             }
+         }*/
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.getPagingFlow(requireContext()).observe(viewLifecycleOwner, { pagingData ->
+                pagingAdapter.submitData(
+                    viewLifecycleOwner.lifecycle,
+                    pagingData
+                )
+            })
+        }
+        //Loading state showing
+        viewLifecycleOwner.lifecycleScope.launch {
+            pagingAdapter.loadStateFlow.collectLatest { loadStates ->
+                binding.loader.progressBar.isVisible = loadStates.refresh is LoadState.Loading
+                binding.loader.textLoadingMsg.isVisible = loadStates.refresh is LoadState.Loading
+                binding.loader.loadStateErrorMessage.isVisible =
+                    loadStates.refresh is LoadState.Error
+            }
+        }
+
+
+        binding.searchToolbar.searchview.setOnQueryTextListener(object :
+            SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                binding.recyclerView.scrollToPosition(0)
+                viewModel.getPokemons(query?.trim())
+                binding.searchToolbar.searchview.clearFocus()
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                val query = newText.toString()
+                if (query.length >= 6) {
+                    viewModel.getPokemons(query.trim())
+                }
+                return true
+            }
+        })
 
         return binding.root
     }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 }
+
